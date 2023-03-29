d300ebe414116ee78d6acb611b449566142a4949
BuildTools <unconfigured@null.spigotmc.org>
2023-03-29 13:51:16 +0200

    rotating status

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, Events, GatewayIntentBits, ButtonStyle, ActivityType } = require('discord.js');
+const { Client, Events, GatewayIntentBits, ButtonStyle, ActivityType, PermissionFlagsBits } = require('discord.js');
 require('dotenv').config()
 
 const bot = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildEmojisAndStickers, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers] });
@@ -7,11 +7,25 @@ const command = require('./command');
 
 bot.once(Events.ClientReady, c => {
     console.log(`# logged in as ${c.user.tag}`);
-    bot.user.setActivity('in hell', { type: ActivityType.Streaming, url: 'https://twitch.tv/rinsednreused' });
+
+    const stati = [`to ${c.guilds.cache.size} guilds`, `in hell`]
+    const updDelay = 5;
+    let currIndx = 0;
+
+    setInterval(() => {
+        const status = stati[currIndx];
+        bot.user.setActivity(status, { type: ActivityType.Streaming, url: 'https://twitch.tv/rinsednreused' });
+
+        currIndx = currIndx >= stati.length - 1 
+            ? 0
+            : currIndx++;
+    }, 5000)
 
     command(bot, ['ping', 'test'], (msg) => {
         msg.channel.send(`latency is \`${Date.now() - msg.createdTimestamp}ms\`.`)
     });
+
+
 });
 
 bot.login(process.env.TOKEN);
